76ba87f98ff5a0040bbc3d1a19ccc4db6f0fcbb0
ayrus7012 <suryas7012@gmail.com>
2024-10-23 09:21:26 +0700

    penambahan surya

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+// Menambahkan informasi credentials
+$servername = "localhost"; // 127.0.0.1
+$username = "root";
+$password = "";
+$database = "tugas_database";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Query script SQL database untuk mendapatkan data dari table data_user
+$sql = "SELECT * FROM data_user";
+$hasil = $conn->query($sql);
+while($baris = $hasil->fetch_assoc()) {
+    $user_db = $baris['username'];
+    $pass_db = $baris['password'];
+
+    echo $user_db;
+
+}
+
+?>
