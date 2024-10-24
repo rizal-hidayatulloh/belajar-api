4b0d2b396dcbd096b7ed718aad50316956e330f4
ayrus7012 <suryas7012@gmail.com>
2024-10-24 17:55:58 +0700

    tugas API data barang

--- data_barang.php
+++ data_barang.php
@@ -1,4 +1,10 @@
 <?php
+
+$id_fr      = $_GET['id_barang'];
+$nama_fr    = $_GET['nama_barang'];
+$harga_fr   = $_GET['harga'];
+$stok_fr    = $_GET['stok'];
+
 // Menambahkan informasi credentials
 $servername = "localhost"; // 127.0.0.1
 $username = "root";
@@ -12,16 +18,30 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 $sql = "SELECT * FROM data_barang";
 $hasil = $conn->query($sql);
 while($baris = $hasil->fetch_assoc()) {
-    $id_db = $baris['id_barang'];
-    $nama_db = $baris['nama_barang'];
-    $harga_db = $baris['harga'];
-    $stok_db = $baris ['stok'];
+    $id_db      = $baris['id_barang'];
+    $nama_db    = $baris['nama_barang'];
+    $harga_db   = $baris['harga'];
+    $stok_db    = $baris['stok'];
 
-    echo $id_db;
-    echo $nama_db;
-    echo $harga_db;
-    echo $stok_db;
+    if ($nama_db == $nama_fr){
+        if ($stok_db <= $stok_fr){
+            $status ="Pembelian barang berhasil diinput";
+        }else{
+            $status ="Stok barang tidak mencukupi";
+        }
+    }else{
+        $status = "Kesalahan input mohon periksa ulang";
+    }
 
-}
+    }
+
+
+
+$response = [
+    'nama'      => $nama_db,
+    'stok'      => $stok_db,
+    'status'    => $status,
+];
+echo json_encode($response);
 
 ?>
