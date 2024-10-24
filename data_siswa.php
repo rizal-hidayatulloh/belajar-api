be6e0196e65b8285771da4ea882f15511830a16b
sansan12082005 <sandiinugraha128@gmail.com>
2024-10-24 14:31:10 +0700

    perubahan data siswa

--- data_siswa.php
+++ data_siswa.php
@@ -1,5 +1,7 @@
 <?php
 
+$nama_fr = $_GET['nama'];
+
 // Menambahkan informasi credentials
 $servername = "localhost"; // 127.0.0.1
 $username = "root";
@@ -13,15 +15,23 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 $sql = "SELECT * FROM data_siswa";
 $hasil = $conn->query($sql);
 while($baris = $hasil->fetch_assoc()) {
-    $nama_db = $baris['Nama_sisa'];
-    $alamat_db = $baris['Alamat'];
-    $jenis_db= $baris['Jenis_kelamin'];
-
-    echo $nama_db;
-    echo $alamat_db;
-    echo $jenis_db;
-    break;
+    $nama_db = $baris['nama_siswa'];
+    
+    if ($nama_db== $nama_fr){
+        $status= "nama benar";
+        break;
+    }
+    else {
+        $status="nama tidak benar";
+        break;
+    }
 
 }
 
-// ?>
+$response = [
+    'nama_siswa' => $nama_db,
+    'status' => $status
+];
+echo json_encode($response);
+
+ ?>
